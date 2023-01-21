41375726ec8b678c3a9ada7ccf62e2a39b931a0b
Jim Payne <jim.payne@ametek.com>
2023-01-20 18:48:03 -0600

    Add PythonEngine AllowThreads utility
    Allows cleaner and safer syntax by entering and exiting AllowThreads
    with using statements.

---
+++ Cameca.CustomAnalysis.PythonScript/Python/PyAllowThreads.cs
@@ -0,0 +1,26 @@
+﻿using Python.Runtime;
+using System;
+
+namespace Cameca.CustomAnalysis.PythonScript.Python;
+
+/// <summary>
+/// Disposable wrapper around PythonEngine AllowThreads methods.
+/// Calls <see cref="PythonEngine.BeginAllowThreads()" /> on creation and
+/// <see cref="PythonEngine.EndAllowThreads(IntPtr)" /> on <see cref="IDisposable.Dispose()" />.
+/// Utilization with <c>using</c> statements ensures
+/// <see cref="PythonEngine.EndAllowThreads(IntPtr)" /> is always called.
+/// </summary>
+internal sealed class PyAllowThreads : IDisposable
+{
+	private readonly IntPtr _threadState;
+
+	public PyAllowThreads()
+	{
+		_threadState = PythonEngine.BeginAllowThreads();
+	}
+
+	public void Dispose()
+	{
+		PythonEngine.EndAllowThreads(_threadState);
+	}
+}
