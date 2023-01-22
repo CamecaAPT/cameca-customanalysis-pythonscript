1a6da4362e55eec139c2d25ec3ad2d26eaa31738
Jim Payne <jim.payne@ametek.com>
2023-01-21 21:17:18 -0600

    Add main Python Script analysis view

---
+++ Cameca.CustomAnalysis.PythonScript/PythonScriptAnalysis/IsNullOrEmptyToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Cameca.CustomAnalysis.PythonScript.PythonScriptAnalysis;
+
+internal class IsNullOrEmptyToVisibilityConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is not string strValue) return DependencyProperty.UnsetValue;
+		return string.IsNullOrEmpty(strValue) ? Visibility.Visible : Visibility.Collapsed;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
